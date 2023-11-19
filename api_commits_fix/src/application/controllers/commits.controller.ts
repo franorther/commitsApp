5cc0497620b2304c284e5cb844072359015fe391
Francisco Ortega <francisco.ortega@softlond.co>
2023-11-19 00:13:11 -0500

    Promise<DataOutGetComitstDTO> added to controller

--- api_commits_fix/src/application/controllers/commits.controller.ts
+++ api_commits_fix/src/application/controllers/commits.controller.ts
@@ -1,13 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Query } from '@nestjs/common';
 import { CommitsService } from '../../domain/services';
-import { DataEntryGetCommitDTO } from 'src/domain/dtos';
+import { DataEntryGetCommitDTO, DataOutGetComitstDTO } from 'src/domain/dtos';
 
 @Controller('commits')
 export class CommitsController {
-  constructor(private readonly commitsService: CommitsService) {}
+  constructor(private readonly commitsService: CommitsService) { }
 
   @Get()
-  getCommits(@Query() data: DataEntryGetCommitDTO): any {
+  getCommits(@Query() data: DataEntryGetCommitDTO): Promise<DataOutGetComitstDTO> {
     return this.commitsService.getCommitsRecord(data);
   }
 }
